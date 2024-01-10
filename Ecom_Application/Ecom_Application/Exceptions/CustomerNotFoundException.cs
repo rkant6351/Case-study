4d8caa2df7afd01b4f09d8e1dd197ed39165a79e
rkant6351 <153700365+rkant6351@users.noreply.github.com>
2024-01-10 18:09:02 +0530

    Add files via upload

---
+++ Ecom_Application/Ecom_Application/Exceptions/CustomerNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom_Application.Exceptions_classes
+{
+    internal class CustomerNotFoundException:Exception
+    {
+        public CustomerNotFoundException() : base("Customer ID Is invalid")
+        {
+
+        }
+    }
+}
---
+++ Ecom_Application/Ecom_Application/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom_Application.Exceptions_classes
+{
+    internal class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException() : base("Product Id is invalid")
+        {
+
+        }
+    }
+}
